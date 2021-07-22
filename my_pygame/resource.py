bb914837868a999a47ef829c2f4b75b006f82bf9
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@frankysnow.fr>
2021-07-22 12:10:05 +0200

    [UPDATE] Improved Resources management

--- my_pygame/resource.py
+++ my_pygame/resource.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 from re import split as re_split
-from typing import Any, Dict, Generic, List, Tuple, Type, TypeVar, TypedDict, Union
+from typing import Any, Dict, Generic, Iterator, List, Mapping, Tuple, Type, TypeVar, TypedDict, Union
 from abc import ABCMeta, abstractmethod
 
 import pygame.image
@@ -20,6 +20,9 @@ class ResourceLoader(Generic[T], metaclass=ABCMeta):
     def __init__(self, filepath: str) -> None:
         super().__init__()
         self.__filepath: str = set_constant_file(*re_split(r"/|\\", filepath))
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({self.filepath})"
 
     @abstractmethod
     def load(self) -> T:
@@ -61,58 +64,91 @@ class ResourcesFile(TypedDict):
     loader: Type[ResourceLoader[Any]]
 
 
-class MetaResourceManager(type):
-    __resources_files__: Dict[str, ResourcesFile]
+class ResourceManagerFiles(Mapping[str, ResourcesFile]):
+    def __init__(self, default: Dict[str, ResourcesFile]) -> None:
+        super().__init__()
+        self.__default: Dict[str, ResourcesFile] = default
 
+    def __getitem__(self, key: str) -> ResourcesFile:
+        return self.__default[key]
+
+    def __len__(self) -> int:
+        return len(self.__default)
+
+    def __iter__(self) -> Iterator[str]:
+        return iter(self.__default)
+
+
+class MetaResourceManager(type):
     def __new__(metacls, name: str, bases: Tuple[type, ...], namespace: Dict[str, Any], **extra: Any) -> MetaResourceManager:
-        namespace["__resources_files__"] = namespace.get("__resources_files__", dict())
-        namespace["__resources_loader__"] = dict()
+        namespace["__resources_files__"] = ResourceManagerFiles(namespace.get("__resources_files__", dict()))
         return super().__new__(metacls, name, bases, namespace, **extra)
 
     def __init__(cls, name: str, bases: Tuple[type, ...], namespace: Dict[str, Any], **extra: Any) -> None:
         super().__init__(name, bases, namespace, **extra)
-        cls.__resources_loader__: Dict[str, _ResourceLoader] = {
-            name: cls.__get_resources_loader(file["path"], file["loader"]) for name, file in cls.__resources_files__.items()
+
+        def get_resources_loader(path: _ResourcePath, loader: Type[ResourceLoader[Any]]) -> _ResourceLoader:
+            if isinstance(path, str):
+                resource_loader: ResourceLoader[Any] = loader(path)
+                cls.__total_nb_resources += 1
+                return resource_loader
+            if isinstance(path, (list, tuple)):
+                return tuple(get_resources_loader(p, loader) for p in path)
+            if isinstance(path, dict):
+                return {key: get_resources_loader(value, loader) for key, value in path.items()}
+            raise TypeError(f"Unexpected path type: {repr(type(path).__name__)} ({repr(path)})")
+
+        cls.__resources_files__: ResourceManagerFiles
+        cls.__total_nb_resources: int = 0
+        cls.__nb_loaded_resources: int = 0
+        cls.__resources_loader: Dict[str, _ResourceLoader] = {
+            name: get_resources_loader(file["path"], file["loader"]) for name, file in cls.__resources_files__.items()
         }
 
     def __setattr__(cls, name: str, value: Any) -> None:
-        if name in cls.__resources_files__:
+        if name == "__resources_files__" or name in cls.__resources_files__:
             raise AttributeError("Resource attributes can't be set")
         return super().__setattr__(name, value)
 
     def __getattribute__(cls, name: str) -> Any:
-        if name != "__resources_loader__" and name in cls.__resources_loader__:
-            resource_attribute: str = f"_{cls.__name__}__{name}"
-            resource: Any
-            if not hasattr(cls, resource_attribute):
-                resource = cls.__load_all_resources(cls.__resources_loader__[name])
-                setattr(cls, resource_attribute, resource)
-            else:
-                resource = getattr(cls, resource_attribute)
-            return resource
+        if name != "__resources_files__" and name in cls.__resources_files__:
+            return cls.__get_resource(name)
 
         return super().__getattribute__(name)
 
-    @staticmethod
-    def __get_resources_loader(path: _ResourcePath, loader: Type[ResourceLoader[Any]]) -> _ResourceLoader:
-        if isinstance(path, str):
-            return loader(path)
-        if isinstance(path, (list, tuple)):
-            return tuple(MetaResourceManager.__get_resources_loader(p, loader) for p in path)
-        if isinstance(path, dict):
-            return {key: MetaResourceManager.__get_resources_loader(value, loader) for key, value in path.items()}
-        raise TypeError(f"Unexpected path type: {repr(type(path).__name__)}")
+    def get_total_nb_resources(cls) -> int:
+        return cls.__total_nb_resources
 
-    @staticmethod
-    def __load_all_resources(resource_loader: _ResourceLoader) -> Any:
-        if isinstance(resource_loader, ResourceLoader):
-            return resource_loader.load()
-        if isinstance(resource_loader, tuple):
-            return tuple(MetaResourceManager.__load_all_resources(loader) for loader in resource_loader)
-        if isinstance(resource_loader, dict):
-            return {key: MetaResourceManager.__load_all_resources(value) for key, value in resource_loader.items()}
-        raise TypeError(f"Unexpected resourcel loader type: {repr(type(resource_loader).__name__)}")
+    def get_nb_loader_resources(cls) -> int:
+        return cls.__nb_loaded_resources
+
+    def __get_resource(cls, resource_name: str) -> Any:
+        def load_all_resources(resource_loader: _ResourceLoader) -> Any:
+            if isinstance(resource_loader, ResourceLoader):
+                resource: Any = resource_loader.load()
+                cls.__nb_loaded_resources += 1
+                return resource
+            if isinstance(resource_loader, tuple):
+                return tuple(load_all_resources(loader) for loader in resource_loader)
+            if isinstance(resource_loader, dict):
+                return {key: load_all_resources(value) for key, value in resource_loader.items()}
+            raise TypeError(f"Unexpected resource loader type: {repr(type(resource_loader).__name__)} ({repr(resource_loader)})")
+
+        resource_attribute: str = f"_{cls.__name__}__{resource_name}"
+        resource: Any
+        if not hasattr(cls, resource_attribute):
+            resource = load_all_resources(cls.__resources_loader[resource_name])
+            setattr(cls, resource_attribute, resource)
+        else:
+            resource = getattr(cls, resource_attribute)
+        return resource
 
 
 class ResourceManager(metaclass=MetaResourceManager):
-    __resources_files__: Dict[str, ResourcesFile]
+    @property
+    def total_nb_resources(self) -> int:
+        return type(self).get_total_nb_resources()
+
+    @property
+    def nb_loaded_resources(self) -> int:
+        return type(self).get_nb_loader_resources()
