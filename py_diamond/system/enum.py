107b6fbe56a5438fcff9b9f4db8e5f5eb4fc7b76
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2022-04-16 15:48:01 +0200

    [FIX] Fixed type hint for StrEnum

--- py_diamond/system/enum.py
+++ py_diamond/system/enum.py
@@ -12,20 +12,39 @@ __author__ = "Francis Clairicia-Rose-Claire-Josephine"
 __copyright__ = "Copyright (c) 2021-2022, Francis Clairicia-Rose-Claire-Josephine"
 __license__ = "GNU GPL v3.0"
 
+import sys
 from enum import Enum
-from typing import TYPE_CHECKING, Any, TypeVar
+from typing import TYPE_CHECKING, Any
 
-if TYPE_CHECKING:
-    _StrEnumSelf = TypeVar("_StrEnumSelf", bound="StrEnum")
+if sys.version_info >= (3, 11):
+    from enum import StrEnum
 
-
-class StrEnum(str, Enum):
-    value: str
+else:
 
     if TYPE_CHECKING:
+        from _typeshed import Self
 
-        def __new__(cls: type[_StrEnumSelf], value: str | _StrEnumSelf) -> _StrEnumSelf:
-            ...
+    def __int_enum_monkeypatch() -> None:
+        from enum import IntEnum
+
+        setattr(IntEnum, "__str__", int.__str__)
+        setattr(IntEnum, "__format__", int.__format__)
+
+    __int_enum_monkeypatch()
+    del __int_enum_monkeypatch
+
+    class StrEnum(str, Enum):
+        value: str
+
+        if TYPE_CHECKING:
+
+            def __new__(cls: type[Self], value: str | Self) -> Self:
+                ...
+
+        if not TYPE_CHECKING:
+
+            __str__ = str.__str__
+            __format__ = str.__format__
 
 
 class AutoLowerNameEnum(StrEnum):
