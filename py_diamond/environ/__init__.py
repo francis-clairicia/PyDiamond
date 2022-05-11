b63547d1b3b46b7715699af04a7b8020bd090da3
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2022-05-11 08:04:56 +0200

    [ADD] py_diamond.environment: Added 'executable' module

--- py_diamond/environ/__init__.py
+++ py_diamond/environ/__init__.py
@@ -4,7 +4,12 @@
 #
 """PyDiamond's environment module"""
 
-__all__ = ["check_booleans"]
+__all__ = [
+    "check_booleans",
+    "get_executable_path",
+    "get_main_script_path",
+    "is_frozen_executable",
+]
 
 __author__ = "Francis Clairicia-Rose-Claire-Josephine"
 __copyright__ = "Copyright (c) 2021-2022, Francis Clairicia-Rose-Claire-Josephine"
@@ -12,3 +17,4 @@ __license__ = "GNU GPL v3.0"
 
 
 from .check import check_booleans
+from .executable import get_executable_path, get_main_script_path, is_frozen_executable
---
+++ py_diamond/environ/executable.py
@@ -0,0 +1,44 @@
+# -*- coding: Utf-8 -*
+# Copyright (c) 2021-2022, Francis Clairicia-Rose-Claire-Josephine
+#
+#
+"""PyDiamond's environment executable utils module"""
+
+from __future__ import annotations
+
+__all__ = [
+    "get_executable_path",
+    "get_main_script_path",
+    "is_frozen_executable",
+]
+
+__author__ = "Francis Clairicia-Rose-Claire-Josephine"
+__copyright__ = "Copyright (c) 2021-2022, Francis Clairicia-Rose-Claire-Josephine"
+__license__ = "GNU GPL v3.0"
+
+import os.path
+import sys
+
+
+def get_main_script_path() -> str:
+    try:
+        main_module = sys.modules["__main__"]
+    except KeyError as exc:
+        raise ImportError("Wait... How the '__main__' module cannot exist ?") from exc
+
+    spec = main_module.__spec__
+    if spec and spec.origin:
+        path = os.path.realpath(spec.origin)
+    else:
+        path = getattr(main_module, "__file__", None) or os.path.realpath(sys.argv[0])
+    return os.path.abspath(path)
+
+
+def get_executable_path() -> str:
+    if is_frozen_executable():
+        return sys.executable
+    return get_main_script_path()
+
+
+def is_frozen_executable() -> bool:
+    return True if getattr(sys, "frozen", False) else False
