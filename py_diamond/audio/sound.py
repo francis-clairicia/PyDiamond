06d04076ae370b88eb39353be20c0786f1f28577
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2022-07-03 09:45:24 +0200

    [FIX] Removed py_diamond/system/duplicate.py

--- py_diamond/audio/sound.py
+++ py_diamond/audio/sound.py
@@ -8,21 +8,4 @@ from __future__ import annotations
 
 __all__ = ["Channel", "Sound"]
 
-import pygame.mixer
-from pygame import encode_file_path
-from pygame.mixer import Channel
-
-from ..system.duplicate import NoDuplicate
-
-
-class Sound(pygame.mixer.Sound, NoDuplicate):
-    __slots__ = ()
-
-    def __init__(self, file: str | bytes) -> None:
-        if not isinstance(file, bytes):
-            file = encode_file_path(file)
-        return super().__init__(file=file)
-
-    def set_volume(self, value: float) -> None:
-        value = max(value, 0)
-        return super().set_volume(value)
+from pygame.mixer import Channel, Sound
