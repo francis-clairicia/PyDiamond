e5eed8a75d0f147cce4f24852123bf740983d663
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2022-09-23 13:49:48 +0200

    [FIX] pydiamond.system.proxy: Bugfix and improvements
    - ProxyType is now pickleable
    
    - Methods returning self in wrapped objects silently replaced by proxy

--- pydiamond/system/proxy.py
+++ pydiamond/system/proxy.py
@@ -8,15 +8,14 @@ __all__ = [
     "proxy",
 ]
 
-from typing import TYPE_CHECKING, Any, Callable, ClassVar, TypeVar
-from weakref import WeakKeyDictionary, WeakValueDictionary
+from typing import TYPE_CHECKING, Any, Callable, ClassVar, SupportsIndex, TypeVar
+from weakref import WeakKeyDictionary, WeakSet, WeakValueDictionary
 
 # Inspired from https://code.activestate.com/recipes/496741-object-proxying/
 
 _T = TypeVar("_T")
 
 
-# TODO: pickling proxy
 class ProxyType(object):
     __slots__ = ("_obj",)
 
@@ -29,12 +28,21 @@ class ProxyType(object):
     # proxying (special cases)
     #
     def __getattribute__(self, name: str, /) -> Any:
-        return getattr(object.__getattribute__(self, "_obj"), name)
+        if name in ProxyType.__special_names_pickle:
+            return object.__getattribute__(self, name)
+        obj: Any = object.__getattribute__(self, "_obj")
+        value: Any = getattr(obj, name)
+        return value if value is not obj else self
 
-    def __setattr__(self, name: str, value: Any) -> None:
-        setattr(object.__getattribute__(self, "_obj"), name, value)
+    def __setattr__(self, name: str, value: Any, /) -> None:
+        if name in ProxyType.__special_names_pickle:
+            return object.__setattr__(self, name, value)
+        obj: Any = object.__getattribute__(self, "_obj")
+        setattr(obj, name, value if value is not obj else self)
 
     def __delattr__(self, name: str, /) -> None:
+        if name in ProxyType.__special_names_pickle:
+            return object.__delattr__(self, name)
         delattr(object.__getattribute__(self, "_obj"), name)
 
     def __bool__(self) -> bool:
@@ -46,6 +54,20 @@ class ProxyType(object):
     def __repr__(self) -> str:
         obj: object = object.__getattribute__(self, "_obj")
         return f"<{type(self).__qualname__} at {id(self):#x}; to {obj!r}>"
+
+    def __reduce_ex__(self, __protocol: SupportsIndex) -> str | tuple[Any, ...]:
+        proxy_cls: type[Any] = type(self)
+        dynmaic_classes = ProxyType.__dynamic_classes
+        while proxy_cls in dynmaic_classes:
+            proxy_cls = proxy_cls.__base__
+        return proxy_cls, (object.__getattribute__(self, "_obj"),)
+
+    def __reduce__(self) -> str | tuple[Any, ...]:
+        proxy_cls: type[Any] = type(self)
+        dynmaic_classes = ProxyType.__dynamic_classes
+        while proxy_cls in dynmaic_classes:
+            proxy_cls = proxy_cls.__base__
+        return proxy_cls, (object.__getattribute__(self, "_obj"),)
 
     #
     # factories
@@ -129,37 +151,82 @@ class ProxyType(object):
         "__xor__",
     ]
 
+    __special_names_returning_self = [
+        "__iadd__",
+        "__iand__",
+        "__iconcat__",
+        "__idiv__",
+        "__idivmod__",
+        "__ifloordiv__",
+        "__ilshift__",
+        "__imod__",
+        "__imul__",
+        "__index__",
+        "__ior__",
+        "__ipow__",
+        "__irshift__",
+        "__isub__",
+        "__itruediv__",
+        "__ixor__",
+    ]
+
+    # fmt: off
+    assert not (set(__special_names_returning_self) - set(__special_names)), (set(__special_names_returning_self) - set(__special_names))
+    # fmt: on
+
+    __special_names_pickle = [
+        "__getnewargs__",
+        "__getnewargs_ex__",
+        "__getstate__",
+        "__reduce__",
+        "__reduce_ex__",
+        "__setstate__",
+    ]
+
     @classmethod
-    def __create_class_proxy(cls, theclass: type[Any]) -> type[ProxyType]:
+    def __create_class_proxy(cls, theclass: type[Any], *, callable: bool) -> type[ProxyType]:
         """creates a proxy for the given class"""
 
         from types import new_class
 
         def make_method(name: str) -> Callable[..., Any]:
-            def method(self: object, *args: Any, **kwargs: Any) -> Any:
-                return getattr(object.__getattribute__(self, "_obj"), name)(*args, **kwargs)
+            if name not in ProxyType.__special_names_returning_self:
+
+                def method(self: object, *args: Any, **kwargs: Any) -> Any:
+                    return getattr(object.__getattribute__(self, "_obj"), name)(*args, **kwargs)
+
+            else:
+
+                def method(self: object, *args: Any, **kwargs: Any) -> Any:
+                    proxy_cls: type[Any] = type(self)
+                    return proxy_cls(getattr(object.__getattribute__(self, "_obj"), name)(*args, **kwargs))
 
             return method
 
         cls_name: str = f"{cls.__name__}({theclass.__name__})"
+        cls_bases: tuple[object, ...] = (cls,)
+        if callable and not issubclass(cls, CallableProxyType):
+            if issubclass(CallableProxyType, cls):
+                cls_bases = (CallableProxyType,)
+            else:
+                cls_bases = (cls, CallableProxyType)
 
         def exec_body(namespace: dict[str, Any]) -> None:
             namespace["__class__"] = property(lambda _: theclass)
             namespace["__qualname__"] = cls_name
             namespace["__module__"] = __name__
             namespace["__slots__"] = ()
-            for name in cls.__special_names:
+            for name in ProxyType.__special_names:
                 if hasattr(theclass, name):
                     namespace[name] = make_method(name)
             if not hasattr(cls, "__weakref__") and hasattr(theclass, "__weakref__"):
                 namespace["__slots__"] += ("__weakref__",)
-            elif not hasattr(theclass, "__weakref__") and hasattr(cls, "__weakref__"):
-                raise TypeError(f"{cls.__qualname__} supports weak references while real object {cls.__qualname__} does not.")
 
-        return new_class(cls_name, (cls,), exec_body=exec_body)
+        return new_class(cls_name, cls_bases, exec_body=exec_body)
 
     __class_proxy_cache: ClassVar[WeakKeyDictionary[type[ProxyType], WeakValueDictionary[type[Any], type[ProxyType]]]]
     __class_proxy_cache = WeakKeyDictionary()
+    __dynamic_classes: WeakSet[type[ProxyType]] = WeakSet()
 
     def __new__(cls, obj: Any, /, *args: Any, **kwargs: Any) -> Any:
         """
@@ -169,6 +236,8 @@ class ProxyType(object):
         note: _class_proxy_cache is unique per deriving class (each deriving
         class must hold its own cache)
         """
+        while cls in ProxyType.__dynamic_classes:
+            cls = cls.__base__
         cache: WeakValueDictionary[type[Any], type[ProxyType]]
         try:
             cache = ProxyType.__class_proxy_cache[cls]
@@ -177,8 +246,9 @@ class ProxyType(object):
         try:
             theclass = cache[obj.__class__]
         except KeyError:
-            cache[obj.__class__] = theclass = cls.__create_class_proxy(obj.__class__)
+            cache[obj.__class__] = theclass = cls.__create_class_proxy(obj.__class__, callable=callable(obj))
             ProxyType.__class_proxy_cache[theclass] = WeakValueDictionary({obj.__class__: theclass})
+            ProxyType.__dynamic_classes.add(theclass)
         return object.__new__(theclass)
 
 
@@ -194,15 +264,14 @@ class CallableProxyType(ProxyType):
             ...
 
     def __call__(__self, /, *args: Any, **kwargs: Any) -> Any:
-        func: Callable[..., Any] = object.__getattribute__(__self, "_obj")
-        return func(*args, **kwargs)
+        obj: Callable[..., Any] = object.__getattribute__(__self, "_obj")
+        value: Any = obj(*args, **kwargs)
+        return value if value is not obj else __self
 
 
-def proxy(obj: _T, *, proxy_cls: type[ProxyType] | None = None) -> _T:
+def proxy(obj: _T, proxy_cls: type[ProxyType] | None = None) -> _T:
     if proxy_cls is not None:
         if not issubclass(proxy_cls, ProxyType):
             raise TypeError("proxy_cls must be a ProxyType subclass")
         return proxy_cls(obj)  # type: ignore[return-value]
-    if callable(obj):
-        return CallableProxyType(obj)  # type: ignore[return-value]
     return ProxyType(obj)  # type: ignore[return-value]
