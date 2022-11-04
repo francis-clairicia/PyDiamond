1df566e67c3aff896e8c8284a408248a9e57680c
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2022-11-04 21:51:28 +0100

    [FIX] pydiamond.system.singleton improvements/bugfix

--- pydiamond/system/singleton.py
+++ pydiamond/system/singleton.py
@@ -20,6 +20,7 @@ _T = TypeVar("_T")
 class SingletonMeta(ObjectMeta):
     def __new__(mcs, name: str, bases: tuple[type, ...], namespace: dict[str, Any], **kwargs: Any) -> SingletonMeta:
         kwargs.pop("abstract", None)
+        kwargs.pop("lazy", None)
 
         call_twice_error_wrapper = mcs.__call_twice_error_wrapper
 
@@ -31,16 +32,30 @@ class SingletonMeta(ObjectMeta):
         return super().__new__(mcs, name, bases, namespace, **kwargs)
 
     def __init__(
-        cls, name: str, bases: tuple[type, ...], namespace: dict[str, Any], *, abstract: bool = False, **kwargs: Any
+        cls,
+        name: str,
+        bases: tuple[type, ...],
+        namespace: dict[str, Any],
+        *,
+        abstract: bool = False,
+        lazy: bool = False,
+        **kwargs: Any,
     ) -> None:
         super().__init__(name, bases, namespace, **kwargs)
         cls.__abstractsingleton__: bool = bool(abstract or isabstractclass(cls))
         if not cls.__abstractsingleton__:
-            instance = cls()
-            setattr(cls, "_singleton_instance_", instance)
+            final(cls)
+            setattr(cls, "_singleton_instance_", None)
+            if not lazy:
+                cls()
+        elif lazy:
+            raise ValueError("Cannot set 'lazy' parameter for abstract singletons")
+
+    def __call__(cls) -> Any:
+        return cls.instance
 
     def __setattr__(cls, name: str, value: Any, /) -> None:
-        if name in ("_singleton_instance_", "__abstractsingleton__") and name in cls.__dict__:
+        if name in ("_singleton_instance_", "__abstractsingleton__") and name in vars(cls):
             if name == "_singleton_instance_":
                 raise TypeError("Cannot modify singleton instance")
             raise AttributeError(f"{name} is a read-only attribute")
@@ -49,7 +64,7 @@ class SingletonMeta(ObjectMeta):
         return super().__setattr__(name, value)
 
     def __delattr__(cls, name: str, /) -> None:
-        if name in ("_singleton_instance_", "__abstractsingleton__") and name in cls.__dict__:
+        if name in ("_singleton_instance_", "__abstractsingleton__") and name in vars(cls):
             if name == "_singleton_instance_":
                 raise TypeError("Cannot modify singleton instance")
             raise AttributeError(f"{name} is a read-only attribute")
@@ -61,9 +76,13 @@ class SingletonMeta(ObjectMeta):
     @final
     def instance(cls: type[_T]) -> _T:
         try:
-            instance: _T = getattr(cls, "_singleton_instance_")
-        except AttributeError:
-            raise TypeError(f"{cls.__name__} cannot be instantiated") from None
+            instance: _T | None = vars(cls)["_singleton_instance_"]
+        except KeyError:
+            raise TypeError(f"{cls.__qualname__} cannot be instantiated") from None
+        if instance is None:
+            instance = cls.__new__(cls)
+            cls.__init__(instance)
+            super(SingletonMeta, cls).__setattr__("_singleton_instance_", instance)  # type: ignore[misc]
         return instance
 
     class __call_twice_error_wrapper:
@@ -75,12 +94,12 @@ class SingletonMeta(ObjectMeta):
             return self.__func__
 
         def __call__(self, __cls_or_self: Any, /, *args: Any, **kwargs: Any) -> Any:
-            cls: type = __cls_or_self if isinstance(__cls_or_self, type) else type(__cls_or_self)
+            cls: Any = __cls_or_self if isinstance(__cls_or_self, type) else type(__cls_or_self)
             if not isinstance(cls, SingletonMeta):
                 raise TypeError("Called from a non-singleton class")
             if cls.__abstractsingleton__:
-                raise TypeError(f"{cls.__name__} cannot be instantiated")
-            if "_singleton_instance_" in cls.__dict__:
+                raise TypeError(f"{cls.__qualname__} cannot be instantiated")
+            if "_singleton_instance_" in vars(cls) and vars(cls)["_singleton_instance_"] is not None:
                 raise TypeError("Cannot instantiate a singleton twice")
             func = self.__func__
             return func(__cls_or_self, *args, **kwargs)
