dc6302ed875994f66fa9de0b3599b4bb551a9dd9
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2022-12-12 22:25:13 +0100

    [TEST] Improved PEP561 test

--- tests/test_pep.py
+++ tests/test_pep.py
@@ -2,9 +2,10 @@
 
 from __future__ import annotations
 
+import importlib
+import itertools
 import os
-import os.path
-import sys
+import pkgutil
 from typing import TYPE_CHECKING
 
 import pytest
@@ -17,16 +18,25 @@ if TYPE_CHECKING:
 class TestPEP561:
     def test____py_typed____exists_in_all_packages(self, pydiamond_rootdirs_list: list[Path]) -> None:
         # Arrange
-        folder_without_py_typed_file: set[str] = set()
-        pycache_prefix = sys.pycache_prefix or "__pycache__"
+        packages_without_py_typed_file: set[str] = set()
 
         # Act
-        for pydiamond_rootdir in pydiamond_rootdirs_list:
-            for root, _, files in os.walk(pydiamond_rootdir):
-                if os.path.basename(root) == pycache_prefix:
-                    continue
-                if "py.typed" not in files:
-                    folder_without_py_typed_file.add(os.path.relpath(root, str(pydiamond_rootdir.parent)))
+        for directory in itertools.chain.from_iterable(
+            map(
+                lambda module: (
+                    (module_spec.submodule_search_locations if (module_spec := module.__spec__) else module.__path__) or []
+                ),
+                map(
+                    lambda module_info: importlib.import_module(module_info.name),
+                    filter(
+                        lambda module_info: module_info.ispkg,
+                        pkgutil.walk_packages(map(os.fspath, pydiamond_rootdirs_list), prefix="pydiamond."),
+                    ),
+                ),
+            )
+        ):
+            if not os.path.isfile(os.path.join(directory, "py.typed")):
+                packages_without_py_typed_file.add(directory)
 
         # Assert
-        assert not folder_without_py_typed_file
+        assert not packages_without_py_typed_file
