89c84d424a99545cd0e28bc19a5e281691de055a
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2022-12-12 08:52:46 +0100

    [FIX] Test PEP561: Handle custom pycache directory

--- tests/test_pep.py
+++ tests/test_pep.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import os
 import os.path
+import sys
 from typing import TYPE_CHECKING
 
 import pytest
@@ -88,11 +89,12 @@ class TestPEP561:
     def test____py_typed____exists_in_all_packages(self, pydiamond_rootdirs_list: list[Path]) -> None:
         # Arrange
         folder_without_py_typed_file: set[str] = set()
+        pycache_prefix = sys.pycache_prefix or "__pycache__"
 
         # Act
         for pydiamond_rootdir in pydiamond_rootdirs_list:
             for root, _, files in os.walk(pydiamond_rootdir):
-                if os.path.basename(root) == "__pycache__":
+                if os.path.basename(root) == pycache_prefix:
                     continue
                 if "py.typed" not in files:
                     folder_without_py_typed_file.add(os.path.relpath(root, str(pydiamond_rootdir.parent)))
